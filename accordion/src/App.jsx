75050db27bf1675cb557af9aac97a5a071ada02b
Amit Paudel <amitpaudel789@gmail.com>
2025-04-26 22:48:50 +0545

    setup

---
+++ accordion/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="h-screen w-screen flex-col justify-center items-center">
+      <h1 className=" text-amber-50 inline-block text-3xl text-center font-bold border-2 p-6 bg-[#644100]">Enable Multi Selection</h1>
+      <div className="h-[10%] w-[50%] text-amber-50 text-3xl text-center font-bold border-2 p-6 bg-[#644100]">
+        <button>
+          What are accordion components? <span>+</span>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
