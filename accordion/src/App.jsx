82b1aced404c857e9d668c4b1897df685a754c18
Amit Paudel <amitpaudel789@gmail.com>
2025-04-28 18:36:27 +0545

    toggle selection

--- accordion/src/App.jsx
+++ accordion/src/App.jsx
@@ -1,12 +1,47 @@
+import { useState } from "react";
 import Card from "./components/Card";
 
 function App() {
-  const titles = ["What are accordion components?", "What are they used for?", "Accordion as a musical instrument", "Can I create an accordion component with a different framework?"];
+  const titles = [
+    {
+      Q: "What are accordion components?",
+      A: `Accordion components are user interface elements used for
+        organizing and presenting content in a collapsible manner. They
+        typically consist of a header, content, and an expand/collapse
+        action`,
+    },
+    {
+      Q: "What are accordion components2?",
+      A: `Accordion components are user interface elements used for
+        organizing and presenting content in a collapsible manner. They
+        typically consist of a header, content, and an expand/collapse
+        action`,
+    },
+    {
+      Q: "What are accordion components3?",
+      A: `Accordion components are user interface elements used for
+        organizing and presenting content in a collapsible manner. They
+        typically consist of a header, content, and an expand/collapse
+        action`,
+    },
+    {
+      Q: "What are accordion components4?",
+      A: `Accordion components are user interface elements used for
+        organizing and presenting content in a collapsible manner. They
+        typically consist of a header, content, and an expand/collapse
+        action `,
+    },
+  ];
+  const [condition, setCondition] = useState(null);
+
+  function handleClick(value) {
+    setCondition(value === condition ? null : value);
+  }
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center gap-3">
       <div className="h-19 w-[20%] text-amber-50  text-2xl text-center  font-bold border-2 p-6 bg-[#644100]">Enable Multi Selection</div>
       {titles.map((title) => {
-        return <Card key={title} title={title}></Card>;
+        return <Card key={title.Q} title={title.Q} ans={title.A} handleClick={() => handleClick(title.Q)} isOpen={condition === title.Q}></Card>;
       })}
     </div>
   );
--- accordion/src/components/Card.jsx
+++ accordion/src/components/Card.jsx
@@ -1,8 +1,14 @@
-const Card = ({ title }) => {
+const Card = ({ title, ans, handleClick, isOpen }) => {
   return (
-    <div className="flex justify-between h-23 w-[50%] text-amber-50 text-2xl text-center font-bold border-2 p-6 bg-[#644100]">
-      <span>{title}</span>
-      <button>+</button>
+    <div className="flex flex-col justify-between h-auto w-[50%] text-amber-50  text-center  border-2 p-6 bg-[#644100]">
+      <div className="flex justify-between text-2xl font-bold">
+        <span>{title}</span>
+        <button onClick={handleClick}>+</button>
+      </div>
+
+      <div className="mt-3">
+        <p>{isOpen ? ans : ""}</p>
+      </div>
     </div>
   );
 };
