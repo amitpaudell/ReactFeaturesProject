cc462d64f43978b15c2f18bc1001864c8bec5805
Amit Paudel <amitpaudel789@gmail.com>
2025-04-29 22:51:10 +0545

    new change

--- accordion/src/App.jsx
+++ accordion/src/App.jsx
@@ -5,14 +5,14 @@ function App() {
   const titles = [
     {
       Q: "What are accordion components?",
-      A: `Accordion components are user interface elements used for
-        organizing and presenting content in a collapsible manner. They
-        typically consist of a header, content, and an expand/collapse
-        action`,
+      A: `Accordion components are UI elements used to show and hide sections of related content in a compact way. They consist of a list of items that can be individually expanded or collapsed to reveal or hide more information.`,
     },
     {
-      Q: "What are they used for?",
-      A: `Accordion components are used to organize and display large amounts of content in a compact, collapsible format. They help improve user experience by reducing clutter on the page. Common uses include FAQs, settings menus, and lists with detailed descriptions. Users can click to expand or collapse sections as needed. This makes navigation more efficient and content more accessible.`,
+      Q: "What are they used?",
+      A: `Accordion components are user interface elements used for
+      organizing and presenting content in a collapsible manner. They
+      typically consist of a header, content, and an expand/collapse
+      action`,
     },
     {
       Q: "What are accordion components3?",
