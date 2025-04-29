ee47ba07bfb654723eaa5dffaedb7442197fd7e5
Amit Paudel <amitpaudel789@gmail.com>
2025-04-29 22:55:10 +0545

    last

--- accordion/src/App.jsx
+++ accordion/src/App.jsx
@@ -19,11 +19,8 @@ function App() {
       A: `The accordion is a portable, box-shaped musical instrument that produces sound by pushing air through reeds using a bellows. It has a keyboard or buttons on one side (right hand) for playing melodies and bass buttons on the other (left hand) for chords and accompaniment. Sound is created as the bellows are expanded or compressed while pressing keys or buttons. It’s commonly used in folk, classical, and popular music around the world.`,
     },
     {
-      Q: "What are accordion components4?",
-      A: `Accordion components are user interface elements used for
-        organizing and presenting content in a collapsible manner. They
-        typically consist of a header, content, and an expand/collapse
-        action `,
+      Q: "Can I create an accordion component with different framework??",
+      A: `Yes, you can create an accordion component using various frameworks like React, Vue.js, Angular, Svelte, or even plain HTML/CSS with JavaScript. Each framework offers different tools for managing state and toggling visibility. Libraries like Bootstrap or Material UI provide pre-built accordion components for faster development. These components can be customized in style and behavior to fit your needs. The flexibility of modern frameworks makes implementing accordions both easy and efficient. `,
     },
   ];
   const [condition, setCondition] = useState(null);
