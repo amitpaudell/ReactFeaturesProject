3d5393918d04070fa2e180894117635f58f36762
Amit Paudel <amitpaudel789@gmail.com>
2025-04-30 23:37:53 +0545

    each card

--- accordion/src/components/Card.jsx
+++ accordion/src/components/Card.jsx
@@ -1,5 +1,6 @@
 const Card = ({ title, ans, handleClick, isOpen }) => {
   return (
+    // Each card component
     <div className="flex flex-col justify-between h-auto w-[50%] text-amber-50  text-center  border-2 p-6 bg-[#644100]">
       <div className="flex justify-between text-2xl font-bold">
         <span>{title}</span>
