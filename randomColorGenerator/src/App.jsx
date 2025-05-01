c74a0d1f10927572e00b68a00d3fc6cf48eec0a4
Amit Paudel <amitpaudel789@gmail.com>
2025-05-01 12:50:02 +0545

    random generator code

--- randomColorGenerator/src/App.jsx
+++ randomColorGenerator/src/App.jsx
@@ -1,13 +1,43 @@
-import "./App.css";
-import ColorPage from "./components/ColorPage";
-function App() {
-  return (
-    <div className="flex justify-center">
-      <button className="bg-blue-700 text-amber-100 p-5 text-2xl m-5 cursor-pointer">Create Hex Color</button>
-      <button className="bg-blue-700 text-amber-100 p-5 text-2xl m-5 cursor-pointer">Create RGB Color</button>
-      <button className="bg-blue-700 text-amber-100 p-5 text-2xl m-5 cursor-pointer">Generate Random Color</button>
+import { useState } from "react";
 
-      <ColorPage></ColorPage>
+function App() {
+  const [colorType, setColorType] = useState("Hex");
+  const [hexColor, setHexColor] = useState("#ffffff");
+  const [rgbColor, setRgbColor] = useState("rgb(255,255,255)");
+
+  function hexrandomColor() {
+    const number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
+    let hex = "#";
+    for (let i = 0; i < 6; i++) {
+      let randomNumber = Math.floor(Math.random() * number.length);
+      hex += number[randomNumber];
+    }
+    setHexColor(hex);
+  }
+
+  function rgbrandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    const rgb = `rgb(${r},${g},${b})`;
+    setRgbColor(rgb);
+  }
+
+  return (
+    <div className="h-screen flex flex-col">
+      <div className="flex justify-center">
+        <button className="bg-blue-700 text-amber-100 p-5 text-2xl m-5 cursor-pointer" onClick={() => setColorType("Hex")}>
+          Create Hex Color
+        </button>
+        <button className="bg-blue-700 text-amber-100 p-5 text-2xl m-5 cursor-pointer" onClick={() => setColorType("Rgb")}>
+          Create RGB Color
+        </button>
+        <button className="bg-blue-700 text-amber-100 p-5 text-2xl m-5 cursor-pointer" onClick={hexrandomColor}>
+          Generate Random Color
+        </button>
+      </div>
+      <h1 className="text-7xl text-center font-bold">{colorType} Color</h1>
     </div>
   );
 }
--- randomColorGenerator/src/components/ColorPage.jsx
+++ randomColorGenerator/src/components/ColorPage.jsx
@@ -1,5 +1,23 @@
-const ColorPage = () => {
-  return <></>;
+import { useEffect, useState } from "react";
+
+const ColorPage = ({ colorFormat }) => {
+  const [randomColor, setRandomColor] = useState({});
+  useEffect(() => {
+    const firstNumber = Math.floor(Math.random() * 255);
+    const secondNumber = Math.floor(Math.random() * 255);
+    const thirdnumber = Math.floor(Math.random() * 255);
+    console.log(firstNumber);
+    console.log(secondNumber);
+    console.log(thirdnumber);
+  }, [colorFormat]);
+  return (
+    <div>
+      <h1 className="text-7xl text-center font-bold">{colorFormat} Color</h1>
+      {/* <p>
+        rgb{randomColor.firstNumber}, {randomColor.secondNumber}, {randomColor.thirdNumber}
+      </p> */}
+    </div>
+  );
 };
 
 export default ColorPage;
