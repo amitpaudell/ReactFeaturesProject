e90e345100251ec4a8a1b9708656eb92be2d0236
Amit Paudel <amitpaudel789@gmail.com>
2025-05-06 19:18:23 +0545

    problem solved

--- imageSlider/src/App.jsx
+++ imageSlider/src/App.jsx
@@ -1,25 +1,50 @@
 import { FaArrowCircleRight } from "react-icons/fa";
 import { FaArrowAltCircleLeft } from "react-icons/fa";
 import { GoDot } from "react-icons/go";
+import { GoDotFill } from "react-icons/go";
 import data from "./data/data";
+import { useState } from "react";
 
 function App() {
+  const [index, setIndex] = useState(1);
+  function handleBackwardClick() {
+    setIndex(index - 1);
+    if (index == 1) {
+      setIndex(data.length);
+    }
+  }
+  function handleForwardClick() {
+    setIndex(index + 1);
+    if (index == data.length) {
+      setIndex(1);
+    }
+  }
+
   return (
     <>
-      <h1 className="text-4xl text-center">Image Slider</h1>
+      <h1 className="text-4xl text-center mb-9">Image Slider</h1>
 
       <div className="relative h-50 w-50 bg-yellow-500 mx-auto mt-4">
         {/* Arrows (one set only) */}
-        <FaArrowAltCircleLeft className="absolute left-2 top-[50%] text-white text-2xl cursor-pointer" />
-        <FaArrowCircleRight className="absolute right-2 top-[50%] text-white text-2xl cursor-pointer" />
+        <FaArrowAltCircleLeft onClick={handleBackwardClick} className="z-1 absolute left-2 top-[50%] text-white text-2xl cursor-pointer" />
+        <FaArrowCircleRight onClick={handleForwardClick} className="z-1 absolute right-2 top-[50%] text-white text-2xl cursor-pointer" />
 
         {/* Dots container */}
-        <div className="absolute bottom-4 left-0 right-0 flex justify-center">
+        <div>
           {data.map((item) => (
-            <GoDot key={item.id} className="text-xl text-white mx-2" />
+            <div key={item.id} className="w-full h-full relative overflow-hidden">
+              {item.id == index ? <img src={item.id == index ? item.img : null} alt="" className="z-0 w-full h-full object-cover" /> : null}
+              {/* <img src={item.id == index ? item.img : null} alt="" className="z-0 w-full h-full object-cover" /> */}
+              <GoDotFill className={`text-xl mx-2 ${index == item.id ? "text-white" : "text-gray-400"}`} />
+            </div>
           ))}
         </div>
       </div>
+
+      {/* <div className="border-1 relative">
+        <img src={data[0].img} alt="" className=" w-20 h-20" />
+        <h1 className="absolute bottom-0">hello</h1>
+      </div> */}
     </>
   );
 }
