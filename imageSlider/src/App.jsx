149d9fe78276e71533ae074d239841e9b03f55e9
Amit Paudel <amitpaudel789@gmail.com>
2025-05-06 17:25:31 +0545

    structure completed

--- imageSlider/src/App.jsx
+++ imageSlider/src/App.jsx
@@ -1,17 +1,25 @@
 import { FaArrowCircleRight } from "react-icons/fa";
 import { FaArrowAltCircleLeft } from "react-icons/fa";
 import { GoDot } from "react-icons/go";
+import data from "./data/data";
+
 function App() {
   return (
     <>
       <h1 className="text-4xl text-center">Image Slider</h1>
 
-      <div className="h-70 w-70 bg-yellow-500 ">
-        <FaArrowAltCircleLeft />
-        <FaArrowCircleRight />
-      </div>
+      <div className="relative h-50 w-50 bg-yellow-500 mx-auto mt-4">
+        {/* Arrows (one set only) */}
+        <FaArrowAltCircleLeft className="absolute left-2 top-[50%] text-white text-2xl cursor-pointer" />
+        <FaArrowCircleRight className="absolute right-2 top-[50%] text-white text-2xl cursor-pointer" />
 
-      <GoDot />
+        {/* Dots container */}
+        <div className="absolute bottom-4 left-0 right-0 flex justify-center">
+          {data.map((item) => (
+            <GoDot key={item.id} className="text-xl text-white mx-2" />
+          ))}
+        </div>
+      </div>
     </>
   );
 }
---
+++ imageSlider/src/data/data.js
@@ -0,0 +1,20 @@
+const data = [
+  {
+    id: 1,
+    img: "https://images.unsplash.com/photo-1746311460525-31a29b35f4c6?q=80&w=2026&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 2,
+    img: "https://plus.unsplash.com/premium_photo-1679064286417-538b9230b5ce?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 3,
+    img: "https://images.unsplash.com/photo-1746483966639-b8dafcd05f5b?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 4,
+    img: "https://images.unsplash.com/photo-1746263665199-24fff5aff18e?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
+
+export default data;
